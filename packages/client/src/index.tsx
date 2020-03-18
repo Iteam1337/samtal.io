09b3eca6d446e0b80664495ee21a87b0ee131473
momentiris <momentiris@gmail.com>
2020-03-18 13:48:11 +0100

    chore: dockerize client

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './routes/App';
-import { ApolloProvider, ApolloClient, split, HttpLink, InMemoryCache } from '@apollo/client';
-import { WebSocketLink } from '@apollo/link-ws'
-import { getMainDefinition } from '@apollo/client/utilities';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./routes/App"
 import {
-  BrowserRouter as Router
-} from 'react-router-dom';
-
+  ApolloProvider,
+  ApolloClient,
+  split,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client"
+import { WebSocketLink } from "@apollo/link-ws"
+import { getMainDefinition } from "@apollo/client/utilities"
+import { BrowserRouter as Router } from "react-router-dom"
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql'
-});
+  uri:
+    process.env.REACT_APP_GRAPHQL_ENDPOINT || "http://localhost:4000/graphql",
+})
 
 const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:4000/graphql',
+  uri:
+    process.env.REACT_APP_GRAPHQL_WS_ENDPOINT || "ws://localhost:4000/graphql",
   options: {
-    reconnect: true
-  }
+    reconnect: true,
+  },
 })
 
 const link = split(
   ({ query }) => {
-    const definition = getMainDefinition(query);
+    const definition = getMainDefinition(query)
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
   },
   wsLink,
-  httpLink,
-);
-
+  httpLink
+)
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -45,7 +49,6 @@ const AppRoot = () => (
       <App />
     </Router>
   </ApolloProvider>
-);
+)
 
-ReactDOM.render(<AppRoot />, document.getElementById('root'));
-
+ReactDOM.render(<AppRoot />, document.getElementById("root"))
