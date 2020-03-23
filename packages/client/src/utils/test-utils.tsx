79e1580732564ab1c7bafed4b3ea8d34e4d859df
Rickard Laurin <rickard.laurin@gmail.com>
2020-03-23 14:53:00 +0100

    test: add login test

---
+++ packages/client/src/utils/test-utils.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { MockedProvider, MockedResponse } from "@apollo/client/testing"
+import { render as rtlRender } from "@testing-library/react"
+import { Router } from "react-router"
+import { createMemoryHistory, MemoryHistory } from "history"
+
+export const render = (
+  ui: JSX.Element,
+  {
+    route = "/",
+    mocks = [],
+    history = createMemoryHistory({ initialEntries: [route] }),
+  }: { route?: string; mocks?: MockedResponse[]; history?: MemoryHistory } = {}
+) => {
+  const Wrapper: React.FC = ({ children }) => (
+    <MockedProvider addTypename={false} mocks={mocks}>
+      <Router history={history}>{children}</Router>
+    </MockedProvider>
+  )
+
+  return {
+    ...rtlRender(ui, { wrapper: Wrapper }),
+    // adding `history` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    history,
+  }
+}
